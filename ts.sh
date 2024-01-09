6b17d37bffad9f152f974bfee3f1523d807eb3ee
Muhammed ALi <155841427+adilali097@users.noreply.github.com>
2024-01-09 21:25:00 +0530

    Update ts.sh

--- ts.sh
+++ ts.sh
@@ -2,6 +2,6 @@
 
 while true; do
     echo "Opening website....."
-    xdg-open ""
+    xdg-open "https://e52d2c6f-4e12-4cf7-be51-bf354026ab47-00-2nctfqowz0una.pike.replit.dev/"
     sleep 10  # Adjust the sleep duration (in seconds) based on your needs
 done
